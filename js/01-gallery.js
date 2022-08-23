4731309c804fb6b6846e9aed6ebf2b5545cd40e9
daryna-s <lypenn@gmail.com>
2022-08-23 22:44:19 +0300

    update task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
+
+const galleryContainer = document.querySelector('.gallery');
+const picturesMarkup = createPictureMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', picturesMarkup);
+
+galleryContainer.addEventListener('click', onGalleryItemsClick);
+
+
+function createPictureMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+   <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" 
+    data-source="${original}" 
+    alt="${description}" />
+  </a>
+</div>;
+    `;
+    })
+    .join('');
+};
+
+function onGalleryItemsClick(evt) {
+    evt.preventDefault();
+  const isPictureEl = evt.target.classList.contains('gallery__item');
+
+  if (!isPictureEl) {
+    return;
+  }
+    const currentPicture = evt.target.dataset.source;
+  const instance = basicLightbox.create(`
+    <img class="modal_image" src="${currentPicture}"/>
+
+`);
+
+  instance.show();
+};
+
 console.log(galleryItems);
