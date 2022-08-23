fc170e9b47482f3de4c404e52c157cd1d9463963
daryna-s <lypenn@gmail.com>
2022-08-23 13:14:19 +0300

    update

--- js/task-05.js
+++
@@ -1,7 +0,0 @@
-const input = document.querySelector('#name-input');
-
-const label = document.querySelector('#name-output');
-
-input.addEventListener('input', event => {
-  label.textContent = event.currentTarget.value ? `${event.currentTarget.value}` : `Anonymous`;
-});
--- js/task-07.js
+++
@@ -1,9 +0,0 @@
-const input = document.querySelector('#font-size-control');
-
-const label = document.querySelector('#text');
-
-input.addEventListener(`input`, onInputChange);
-
-function onInputChange(event) {
-    label.style.fontSize = event.currentTarget.value + 'px';    
-};
